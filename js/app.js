1fec5fd5a6e8eb6d31fcaca15d2eb17e63506a5d
monsterstudios <info@monsterstudios.com.ng>
2019-06-03 16:27:04 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,49 +1,62 @@
-$(function () {
+$(function() {
     var App = {
         /**
          * Init Function
          */
         init: function() {
             App.Sidebar();
+            App.Misc();
         },
 
-        Sidebar: function () {
+        Sidebar: function() {
             $(window).resize(function() {
                 var path = $(this);
                 var contW = path.width();
-                if(contW >= 751){
-                    document.getElementsByClassName("sidebar-toggle")[0].style.left="200px";
-                }else{
-                    document.getElementsByClassName("sidebar-toggle")[0].style.left="-200px";
+                if (contW >= 751) {
+                    document.getElementsByClassName("sidebar-toggle")[0].style.left = "200px";
+                } else {
+                    document.getElementsByClassName("sidebar-toggle")[0].style.left = "-200px";
                 }
             });
 
-            $('.dropdown').on('show.bs.dropdown', function(e){
+            $('.dropdown').on('show.bs.dropdown', function(e) {
                 $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
             });
 
-            $('.dropdown').on('hide.bs.dropdown', function(e){
+            $('.dropdown').on('hide.bs.dropdown', function(e) {
                 $(this).find('.dropdown-menu').first().stop(true, true).slideUp(300);
             });
 
             $(".navbar-toggler").click(function(e) {
                 e.preventDefault();
                 var elem = document.getElementById("sidebar-wrapper");
-                left = window.getComputedStyle(elem,null).getPropertyValue("left");
-                if(left == "200px"){
-                    document.getElementsByClassName("sidebar-toggle")[0].style.left="-200px";
-                }
-                else if(left == "-200px"){
-                    document.getElementsByClassName("sidebar-toggle")[0].style.left="200px";
+                left = window.getComputedStyle(elem, null).getPropertyValue("left");
+                if (left == "200px") {
+                    document.getElementsByClassName("sidebar-toggle")[0].style.left = "-200px";
+                } else if (left == "-200px") {
+                    document.getElementsByClassName("sidebar-toggle")[0].style.left = "200px";
                 }
             });
 
-            function getPageHeight(){
+            function getPageHeight() {
                 var height = (window.innerHeight > 0) ? window.innerHeight : document.documentElement.clientHeight;
-                $('.sidebar-nav').css('height',height-90);
+                $('.sidebar-nav').css('height', height - 90);
             }
             window.onresize = getPageHeight();
             getPageHeight();
+        },
+
+        Misc: function() {
+            $('a[href*="#"]').on('click', function(e) {
+                e.preventDefault()
+
+                $('html, body').animate({
+                        scrollTop: $($(this).attr('href')).offset().top,
+                    },
+                    500,
+                    'linear'
+                )
+            })
         }
     };
 
